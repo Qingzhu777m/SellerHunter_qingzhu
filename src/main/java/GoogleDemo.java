e83fd55c000715d024acb8f81e7d089508b438b0
孟庆柱 <mengqingzhu@moutekiMacBook-Pro.local>
2017-06-06 11:21:21 -0700

    init

---
+++ src/main/java/GoogleDemo.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by mengqingzhu on 6/6/17.
+ */
+public class GoogleDemo {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "webdriver/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+
+
+
+    }
+}
